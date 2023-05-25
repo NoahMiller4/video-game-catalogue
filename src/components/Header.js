042299a7d96794b5b76613f402622414b7ad4686
Noah <noseby@live.ca>
2023-05-25 13:27:22 -0500

    form added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
-import { Component } from 'react'
 
-class Header extends Component {
-  render() {
-    const { title } = this.props;
-    const { linkName } = this.props
-    
-    
-    return (
-        <header>
-            <div className='container flexbox'>
-                <h1>{title}</h1>
-                <div className='inline'>
-                  <button>{linkName}</button>
-                </div>
-            </div>
-        </header>
-    )
-  }
+import React, { useState } from 'react';
+import New from './New';
+
+function Header({ title }) {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleAddGame = () => {
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <header>
+      <div className="container flexbox">
+        <h1>{title}</h1>
+        <div className="inline">
+          <button onClick={handleAddGame}>Add Game</button>
+        </div>
+      </div>
+      {showForm && (
+        <div className="overlay">
+          <div className="dialog">
+            <button className="close" onClick={handleCloseForm}>
+              <p className='x'>&times;</p>
+            </button>
+            <New />
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
--- src/components/New.js
+++ src/components/New.js
@@ -1,9 +1,30 @@
-import React from 'react'
+
+import React from 'react';
 
 function New() {
   return (
-    <div></div>
-  )
+    <div className="new-form">
+      <h2>Add Game</h2>
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" />
+
+        <label htmlFor="year">Year:</label>
+        <input type="text" id="year" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" />
+
+        <label htmlFor="tags">Tags:</label>
+        <input type="text" id="tags" />
+
+        <label htmlFor="url">URL:</label>
+        <input type="text" id="url" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default New
+export default New;
